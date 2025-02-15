b46b083b8c6db4523c3ffecf9e1c9c1de933ec73
ahmadillham <124949959+ahmadillham@users.noreply.github.com>
2025-02-15 20:06:47 +0700

    Add files via upload

---
+++ js/header.js
@@ -0,0 +1,36 @@
+const dynamicText = document.querySelector(".title span");
+const words = [
+  "Chrome",
+  "Edge",
+  "Opera",
+  "Firefox",
+  "VS Code",
+  "Sublime",
+  "XAMPP",
+];
+
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+const typeEffect = () => {
+  const currentWord = words[wordIndex];
+  const currentChar = currentWord.substring(0, charIndex);
+  dynamicText.textContent = currentChar;
+  dynamicText.classList.add("stop-blinking");
+
+  if (!isDeleting && charIndex < currentWord.length) {
+    charIndex++;
+    setTimeout(typeEffect, 100);
+  } else if (isDeleting && charIndex > 0) {
+    charIndex--;
+    setTimeout(typeEffect, 50);
+  } else {
+    isDeleting = !isDeleting;
+    dynamicText.classList.remove("stop-blinking");
+    wordIndex = !isDeleting ? (wordIndex + 1) % words.length : wordIndex;
+    setTimeout(typeEffect, 800);
+  }
+};
+
+typeEffect();
